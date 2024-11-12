d803a2b3e69275433fa911b12ca3d1932b317d1c
suzhetao <suzhetao@gmail.com>
2024-11-12 18:06:11 +0800

    deploy

--- index.ts
+++ index.ts
@@ -299,10 +299,19 @@ const azure_resources = [
                         {
                             addressPrefixes: ["10.30.0.0/23"],
                             subnetName: "snet-p-network-bigdata-eastasia-001",
+                            networkSecurityGroup: {
+                                id: "/subscriptions/b971283c-e0b7-46a4-9496-9cbfb850ebe5/resourceGroups/rg-p-network-container-eastasia-001/providers/Microsoft.Network/networkSecurityGroups/nsg-snet-p-network-bigdata-eastasia-001"
+                            }
                         },
                         {
                             addressPrefixes: ["10.30.2.0/23"],
                             subnetName: "snet-p-network-bigdata-eastasia-002",
+                        }
+                    ],
+                    NetworkSecurityGroup: [
+                        {
+                            networkSecurityGroupName: "nsg-snet-p-network-bigdata-eastasia-001",
+                            securityRules: [],
                         }
                     ],
                     virtualNetworkPeerings: [
